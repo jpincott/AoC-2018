9e814cc876b6aa380dd994bdc3ccf9f84853a740
Jon Pincott <github@pincott.net>
2018-12-17 22:52:04 +0000

    fater

--- src/day14/q1/q1.kt
+++ src/day14/q1/q1.kt
@@ -5,14 +5,6 @@ fun main(args: Array<String>) {
     generateSequence(State(listOf(0, 1), "37")) { s -> s.next() }
         .first { it.recipies.length == 824501 + 10 }
         .let{ println(it.recipies.takeLast(10)) }
-
-    // 1031816654
-
-    generateSequence(State(listOf(0, 1), "37")) { s -> s.next() }
-        .first { it.recipies.takeLast(7).contains("824501") }
-        .also { println(it.recipies) }
-        .also { println(it.recipies.dropLast(6)) }
-        .also { println(it.recipies.dropLast(6).length) }
 }
 
 data class State(val elves: List<Int>, val recipies: String) {
--- src/day14/q2/q2.kt
+++ src/day14/q2/q2.kt
@@ -3,19 +3,34 @@ package day14.q2
 fun main(args: Array<String>) {
 
     val n = 824501
+    val str = "824501"
 
     val elf1 = Recipe(3,1).apply { next = this }.apply { prev = this }
     val elf2 = elf1.add(Recipe(7,2))
     val state = State(elf1, elf2)
 
+//    generateSequence(state) { s -> s.next() }
+//        .first{it.last.idx >= n + 10}
+//        .let { generateSequence(it.last) { r -> r.prev }
+//            .takeWhile { it.idx > n }
+//            .fold("") { acc, r -> r.value.toString() + acc }
+//            .take(10)
+//        }
+//        .let{println(it)}
+
     generateSequence(state) { s -> s.next() }
-        .first{it.last.idx >= n + 10}
-        .let { generateSequence(it.last) { r -> r.prev }
-            .takeWhile { it.idx > n }
-            .fold("") { acc, r -> r.value.toString() + acc }
-            .take(10)
+        .drop(str.length)
+        .first {
+            generateSequence(it.last) { r -> r.prev }
+                .take(str.length + 1)
+                .fold("") { acc, r -> r.value.toString() + acc }
+                .contains(str, true)
         }
-        .let{println(it)}
+        .let {
+            generateSequence(it.last to "${it.last.value}") { (r, s) -> r.prev to r.prev.value.toString() + s }
+                .first { it.second.startsWith(str) }
+        }
+        .let { println(it.first.idx - 1) }
 }
 
 data class State(val elf1:Recipe, val elf2:Recipe, val last:Recipe=elf2) {
