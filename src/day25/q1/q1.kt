bca63aa3cefa838dcd5b353629cc82077211b1c3
Jon Pincott <github@pincott.net>
2019-01-11 18:37:45 +0000

    tidy up

--- src/day25/q1/q1.kt
+++ src/day25/q1/q1.kt
@@ -16,27 +16,15 @@ fun main(args: Array<String>) {
     val constellations = mutableSetOf<MutableSet<Coord>>()
 
     coords.forEach { star ->
-        val nearby = constellations.filter { constellation -> constellation.any {it.dist(star) <= 3} }.toSet()
-        constellations.removeAll(nearby)
-        val element = nearby.flatten().plus(star).toMutableSet()
-        constellations.add(element)
+        val containing = constellations.filter { constellation -> constellation.any { it.dist(star) <= 3 } }.toSet()
+        val union = containing.flatten().plus(star).toMutableSet()
+        constellations.removeAll(containing)
+        constellations.add(union)
     }
 
     println("There are ${constellations.size} constellations")
 }
 
-data class Coord(val x: Int, val y: Int, val z: Int, val t: Int) {
-    constructor(l: List<Int>) : this(l[0], l[1], l[2], l[3])
-
-    val neighbours = mutableSetOf<Coord>()
-    fun add(c: Coord): Boolean = neighbours.add(c)
-
-    fun asList(): List<Int> = listOf(x, y, z, t)
-    fun dist(c: Coord): Int = asList().zip(c.asList()).sumBy { abs(it.first - it.second) }
-
-    fun chain(): MutableSet<Coord> {
-        val elements: List<Coord> = neighbours.map { it.chain() }.flatten()
-        return mutableSetOf(this, *elements.toTypedArray())
-    }
-
+data class Coord(val v: List<Int>) {
+    fun dist(c: Coord): Int = v.zip(c.v).sumBy { abs(it.first - it.second) }
 }
